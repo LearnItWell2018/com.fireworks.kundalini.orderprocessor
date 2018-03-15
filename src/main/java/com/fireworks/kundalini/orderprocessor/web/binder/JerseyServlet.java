41d927e214c4544e7e4541e1ceca359bdee5c49f
arkasuparna <LearnItWell2018@gmail.com>
2018-03-15 10:02:54 +0530

    Cross domain access true

--- src/main/java/com/fireworks/kundalini/orderprocessor/web/binder/JerseyServlet.java
+++ src/main/java/com/fireworks/kundalini/orderprocessor/web/binder/JerseyServlet.java
@@ -6,7 +6,8 @@ import javax.servlet.annotation.WebServlet;
 import com.sun.jersey.spi.spring.container.servlet.SpringServlet;
 
 @WebServlet(urlPatterns = { "/rs/*" }, initParams = { @WebInitParam(name = "com.sun.jersey.config.property.packages", value = "com.fireworks.kundalini.orderprocessor.rest"), 
-		@WebInitParam(name = "com.sun.jersey.api.json.POJOMappingFeature", value = "true")})
+		@WebInitParam(name = "com.sun.jersey.api.json.POJOMappingFeature", value = "true"),
+		@WebInitParam(name = "com.sun.jersey.spi.container.ContainerResponseFilters", value = "com.fireworks.kundalini.orderprocessor.web.binder.CORSFilter")})
 public class JerseyServlet extends SpringServlet {
 
 	/**
