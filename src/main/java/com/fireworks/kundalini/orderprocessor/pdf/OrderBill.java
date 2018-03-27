9b9e6cd92c190299dbb05e50bab56d32bc1f1660
LearnItWell2018 <35795423+LearnItWell2018@users.noreply.github.com>
2018-03-27 18:56:04 +0530

    Coloring Headers

--- src/main/java/com/fireworks/kundalini/orderprocessor/pdf/OrderBill.java
+++ src/main/java/com/fireworks/kundalini/orderprocessor/pdf/OrderBill.java
@@ -7,9 +7,14 @@ import java.util.List;
 import com.fireworks.kundalini.orderprocessor.pojo.customerorder.CustomerOrder;
 import com.fireworks.kundalini.orderprocessor.pojo.customerorder.OrderDetails;
 import com.fireworks.kundalini.orderprocessor.pojo.customerorder.OrderList;
+import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Document;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
 import com.itextpdf.text.Image;
 import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
@@ -27,11 +32,9 @@ public class OrderBill {
 
 			PdfPTable table = new PdfPTable(2);
 			table.addCell(createImageCell("C:/data/WorkSpace/com.fireworks.kundalini.orderprocessor-master/src/main/resources/img/items/bomb.jpg"));
-			table.addCell("Kundalini");
+			table.addCell(fillCustomerDetails(customerOrder));
 			this.document.add(table);
 			fillItemDetails(customerOrder.getOrderDetails().getOrderList());
-			/*HeaderFooterPageEvent event = new HeaderFooterPageEvent();
-			writer.setPageEvent(event);*/
 			document.close();
 			writer.close();
 		} catch (Exception e) {
@@ -44,14 +47,66 @@ public class OrderBill {
 		return new PdfPCell(img, true);
 	}
 
+	private Paragraph fillCustomerDetails(CustomerOrder customerOrder) {
+		Paragraph customerDetails = new Paragraph();
+		
+		Paragraph customerMail = new Paragraph(customerOrder.getCustomerMail());
+		Paragraph customerMobile = new Paragraph(customerOrder.getCustomerMobile());
+		
+		customerDetails.add(customerMail);
+		customerDetails.add(customerMobile);
+		
+		return customerDetails;
+		
+	}
+	
+	private PdfPCell provideType1Cell(String content) {
+		Font font = FontFactory.getFont(FontFactory.COURIER, 8, Font.NORMAL, BaseColor.BLACK);
+		return new PdfPCell(new Phrase(content, font));
+	}
+	
+	private PdfPCell provideType2Cell(String content) {
+		Font font = FontFactory.getFont(FontFactory.TIMES_ROMAN, 12, Font.BOLD, BaseColor.WHITE);
+		PdfPCell cell = new PdfPCell(new Phrase(content, font));
+		cell.setBackgroundColor(BaseColor.BLUE);
+		return cell;
+	}
+	
+	private PdfPCell provideHeaderCell(String content, BaseColor baseColor) {
+		Font font = FontFactory.getFont(FontFactory.defaultEncoding, 10, Font.BOLD, BaseColor.BLACK);
+		PdfPCell cell = new PdfPCell(new Phrase(content, font));
+		cell.setBackgroundColor(baseColor);
+		return cell;
+	}
+	
 	private void fillItemDetails(List<OrderList> orderList) throws Exception {
 		PdfPTable itemTable;
+		
+		itemTable = new PdfPTable(1);
+		itemTable.addCell(provideType2Cell("Order Details"));
+		this.document.add(itemTable);
+		
+		
+		itemTable = new PdfPTable(5);
+		itemTable.setWidths(new float[] { 2, 2, 3, 1, 1});
+		
+		itemTable.addCell(provideHeaderCell("Product Name", BaseColor.ORANGE));
+		itemTable.addCell(provideHeaderCell("Product Image", BaseColor.CYAN));
+		itemTable.addCell(provideHeaderCell("Product Details", BaseColor.PINK));
+		itemTable.addCell(provideHeaderCell("Unit Price", BaseColor.GREEN));
+		itemTable.addCell(provideHeaderCell("Product Count", BaseColor.YELLOW));
+
+		this.document.add(itemTable);
+		
+		
 		for (OrderList order : orderList) {
-			itemTable = new PdfPTable(4);
-			itemTable.addCell(order.getProductId());
+			itemTable = new PdfPTable(5);
+			itemTable.setWidths(new float[] { 2, 2, 3, 1, 1});
+			itemTable.addCell(provideType1Cell(order.getProductId()));
 			itemTable.addCell(createImageCell(order.getItemImage()));
-			itemTable.addCell(order.getItemPrice());
-			itemTable.addCell(order.getItemCount());
+			itemTable.addCell(provideType1Cell(order.getItemDesc()));
+			itemTable.addCell(provideType1Cell(order.getItemPrice()));
+			itemTable.addCell(provideType1Cell(order.getItemCount()));
 			this.document.add(itemTable);
 		}
 
@@ -60,12 +115,17 @@ public class OrderBill {
 	public static void main(String[] args) {
 
 		CustomerOrder customerOrder = new CustomerOrder();
+		
+		customerOrder.setCustomerMail("suparnog13@gmail.com");
+		customerOrder.setCustomerMobile("9051915545");
+		
 		OrderDetails orderDetails = new OrderDetails();
 		List<OrderList> orderList = new ArrayList<>();
 		OrderList orderItem1 = new OrderList();
 
 		orderItem1.setProductId("Product_ID");
 		orderItem1.setItemPrice("80");
+		orderItem1.setItemDesc("Shivaji Rao Gaekwad (born 12 December 1950), known by his mononymous stage name Rajinikanth, is an Indian film actor and politician who works primarily in Tamil cinema. ");
 		orderItem1.setItemImage("C:/data/WorkSpace/com.fireworks.kundalini.orderprocessor-master/src/main/resources/img/items/fountain_l_1.jpg");
 		orderItem1.setItemCount("5");
 		
@@ -73,6 +133,7 @@ public class OrderBill {
 
 		orderItem2.setProductId("Product_ID");
 		orderItem2.setItemPrice("80");
+		orderItem2.setItemDesc("Shivaji Rao Gaekwad (born 12 December 1950), known by his mononymous stage name Rajinikanth, is an Indian film actor and politician who works primarily in Tamil cinema. ");
 		orderItem2.setItemImage("C:/data/WorkSpace/com.fireworks.kundalini.orderprocessor-master/src/main/resources/img/items/shell_4.jpg");
 		orderItem2.setItemCount("5");
 
